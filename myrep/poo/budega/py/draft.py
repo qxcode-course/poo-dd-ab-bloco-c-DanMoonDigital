49bcc7576282ee88c0984a83f9ab7bbed6dd56a9
Dan <pedrodanielmota@gmail.com>
2025-11-11 16:46:02 -0300

    ARRIEGUA BUDEGAAAA

--- myrep/poo/budega/py/draft.py
+++ myrep/poo/budega/py/draft.py
@@ -1,2 +1,41 @@
-class Market
+class Person:
+    def __init__(self, name: str) -> None:
+        self.name = name
+
+    def __str__(self) -> str:
+        return self.name
     
+class Market:
+    def __init__ (self, counter_size: int) -> None:
+        self.counter: list[Person | None] = []
+        for _ in range(counter_size):
+            self.counter.appen(None)
+        self.waiting: list [Person] = []
+
+    def arrive(self, person: Person):
+        self.waiting.append(person)
+
+    def leave(self, index: int) -> Person | None:
+        if index < 0 or index >= len(self.counter):
+            print("Indice invalido")
+            return None
+        if self.counter[index] is None:
+            print("Caixa Vazio")
+            return None
+        aux = self.counter[index]
+        self.counter[index] = None
+        return aux
+
+def main():
+    market = Market(0)
+    while True:
+        line = input()
+        print("$" + line)
+        args = line.split(" ")
+        if args[0] == "end":
+            break
+        elif args [0] == "end":
+            break
+        elif args [0] == "init":
+            qtd = int(args[1])
+            market = Market(qtd)
